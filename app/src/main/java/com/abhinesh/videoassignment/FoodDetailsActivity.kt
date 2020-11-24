af4cf1bdfaec0f47ce4e2d876dec29b97637a1fd
abhineshchandra1234 <52635220+abhineshchandra1234@users.noreply.github.com>
2020-11-24 17:24:32 +0530

    first commit!

--- app/src/main/java/com/abhinesh/videoassignment/FoodDetailsActivity.kt
+++ app/src/main/java/com/abhinesh/videoassignment/FoodDetailsActivity.kt
@@ -15,8 +15,8 @@ class FoodDetailsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_food_details)
 
-//        val mediaController = MediaController(this)
-//        mediaController.setAnchorView(videoView)
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
 
         var bundle = intent.extras
         //imageView2.setImageResource(bundle!!.getInt("image"))
@@ -26,38 +26,38 @@ class FoodDetailsActivity : AppCompatActivity() {
 
         //val onlineUri = Uri.parse(bundle?.getString("des")+"")
 
-        //val offlineUri = Uri.parse("android.resource://$packageName/${R.raw.wakeupsid}")
+        val offlineUri = Uri.parse("android.resource://$packageName/${R.raw.animal}")
 
-//        videoView.setMediaController(mediaController)
-//        videoView.setVideoURI(offlineUri)
-//        videoView.requestFocus()
-//        videoView.start()
+        videoView.setMediaController(mediaController)
+        videoView.setVideoURI(offlineUri)
+        videoView.requestFocus()
+        videoView.start()
 
 
-        third_party_player_view.getPlayerUiController().showFullscreenButton(true)
-        third_party_player_view.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
-            override fun onReady(@NonNull youTubePlayer: YouTubePlayer) {
-                val videoId = bundle?.getString("des")+""    //"YE7VzlLtp-4"
-                youTubePlayer.cueVideo(videoId, 0f)
-            }
-        })
-
-        third_party_player_view.getPlayerUiController().setFullScreenButtonClickListener(View.OnClickListener {
-            if (third_party_player_view.isFullScreen()) {
-                third_party_player_view.exitFullScreen()
-                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
-                // Show ActionBar
-                if (supportActionBar != null) {
-                    supportActionBar!!.show()
-                }
-            } else {
-                third_party_player_view.enterFullScreen()
-                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
-                // Hide ActionBar
-                if (supportActionBar != null) {
-                    supportActionBar!!.hide()
-                }
-            }
-        })
+//        third_party_player_view.getPlayerUiController().showFullscreenButton(true)
+//        third_party_player_view.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
+//            override fun onReady(@NonNull youTubePlayer: YouTubePlayer) {
+//                val videoId = bundle?.getString("des")+""    //"YE7VzlLtp-4"
+//                youTubePlayer.cueVideo(videoId, 0f)
+//            }
+//        })
+//
+//        third_party_player_view.getPlayerUiController().setFullScreenButtonClickListener(View.OnClickListener {
+//            if (third_party_player_view.isFullScreen()) {
+//                third_party_player_view.exitFullScreen()
+//                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
+//                // Show ActionBar
+//                if (supportActionBar != null) {
+//                    supportActionBar!!.show()
+//                }
+//            } else {
+//                third_party_player_view.enterFullScreen()
+//                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+//                // Hide ActionBar
+//                if (supportActionBar != null) {
+//                    supportActionBar!!.hide()
+//                }
+//            }
+//        })
     }
     }
